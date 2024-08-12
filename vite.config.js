c302feecba8e892beb558c5b1d4a6fb689fbc112
Ssuyu Ke <ssuyuke@asy.tw>
2024-08-12 12:41:25 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/travel/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
